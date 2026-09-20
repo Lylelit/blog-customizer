45a89cedfbc5d128e79d225abc7096e7c745d14f
Alena Smirnova <lylelit@yandex.ru>
2026-09-20 10:54:44 +0300

    Первая сборка проекта на первую проверку

---
+++ src/components/article/Article.module.d.scss.ts
@@ -0,0 +1,6 @@
+declare const classNames: {
+  readonly article: "article";
+  readonly titleDescription: "titleDescription";
+  readonly image: "image";
+};
+export default classNames;
---
+++ src/ui/button/Button.module.d.scss.ts
@@ -0,0 +1,6 @@
+declare const classNames: {
+  readonly button: "button";
+  readonly button_apply: "button_apply";
+  readonly button_clear: "button_clear";
+};
+export default classNames;
